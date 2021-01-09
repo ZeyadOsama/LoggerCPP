bdb037ae2e138263032a1bb5c0e388a92601f49f
ZeyadOsama <zeyad.osama@gmail.com>
2021-01-09 18:01:53 +0200

    Modified extentions (*.h -> *.hpp)

--- include/logger/LoggerCPP.hpp
+++ include/logger/LoggerCPP.hpp
@@ -10,7 +10,7 @@
 #include <logger/common/datatypes/Severity.hpp>
 #include <logger/libvars/Extension.hpp>
 #include <logger/libvars/Definitions.hpp>
-#include <logger/common/options/OutputStream.h>
-#include <logger/common/options/Options.h>
+#include <logger/common/options/OutputStream.hpp>
+#include <logger/common/options/Options.hpp>
 
 #endif //LOGGER_LOGGER_CPP_HPP
